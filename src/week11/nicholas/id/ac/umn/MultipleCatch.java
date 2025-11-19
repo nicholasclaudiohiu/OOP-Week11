07e92a333d584f5fd44546ff4940f804eca4f2d7
Nicholas Claudio <nicholasclaudiohiu@gmail.com>
2025-11-19 13:42:31 +0700

    Multiple Catch Implementation

---
+++ src/week11/nicholas/id/ac/umn/MultipleCatch.java
@@ -0,0 +1,23 @@
+package week11.nicholas.id.ac.umn;
+
+import java.util.Scanner;
+public class MultipleCatch {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		try {
+			int n = Integer.parseInt(scn.nextLine());
+			
+			int result = 10/n;
+			System.out.println(result);
+		}
+		catch (ArithmeticException ex) {
+			System.out.println("Arithmetic " + ex);
+		}
+		catch (NumberFormatException ex) {
+			System.out.println("Number Format Exception " + ex);
+		}
+
+	}
+
+}
